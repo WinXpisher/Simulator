5e4c57cee0ed38badf20ce70fa3b6bbccdcdc5d7
Dima Vasylenko <118187404+dima-vs@users.noreply.github.com>
2025-05-07 21:33:53 +0300

    Delete QT/Header files directory

--- QT/Header files/GetTaskStatusStr.h
+++
@@ -1,9 +0,0 @@
-#ifndef GETTASKSTATUSSTR_H
-#define GETTASKSTATUSSTR_H
-
-#include "SimulatorFiles/SimulatorPrimitives.h"
-#include <QString>
-
-QString getTaskStatusStr(const Task& task);
-
-#endif // GETTASKSTATUSSTR_H
