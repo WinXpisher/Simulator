4ce27fb1167b532637574be6fb95235a0cf59561
WinXpisher <yaroslavmax6@gmail.com>
2026-06-01 20:06:22 +0300

    upd: move dist folder to source folder and delete build folder

--- Source/Sim/build/Desktop_Qt_6_10_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_myApp_FILE /home/winxp/Qt_proj/Sim/build/Desktop_Qt_6_10_1-Debug/myApp)
-set(__QT_DEPLOY_TARGET_myApp_TYPE EXECUTABLE)
--- Source/Sim/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_myApp_FILE C:/Qt_projs/Sim/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/myApp.exe)
-set(__QT_DEPLOY_TARGET_myApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_myApp_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
